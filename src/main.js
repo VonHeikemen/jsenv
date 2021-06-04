06e3fc481e370001ea23e7a24e944323bee7c482
Heiker Curiel <heikercuriel@gmail.com>
2021-06-04 11:00:20 -0400

    utils - add more

--- src/main.js
+++ src/main.js
@@ -1,25 +1,10 @@
 const cli = require('arg');
 const sh = require('@util/sh.js');
-
-function stdout(str) {
-  process.stdout.write(str);
-}
-
-async function stdin() {
-  process.stdin.setEncoding('utf8');
-  let input = '';
-
-  for await (const chunk of process.stdin) {
-    input += chunk;
-  }
-
-  return input;
-}
+const io = require('@util/stdio.js');
 
 Object.assign(global, {
   sh,
   cli,
-  stdin,
-  stdout,
+  ...io
 });
 
